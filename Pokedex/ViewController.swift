3f6f8c07f3a40a7789e3ce4f0b4c4006d4621f73
Michelle Kroll <michellekroll@berkeley.edu>
2020-09-28 20:27:28 -0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Michelle Kroll on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
